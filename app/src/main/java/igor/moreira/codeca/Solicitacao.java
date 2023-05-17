e1a41d196a59e6e36a13730f726ccff8e7d0ef91
igormr <igorolimoreira2@gmail.com>
2023-05-16 21:14:01 -0300

    Codeca

---
+++ app/src/main/java/igor/moreira/codeca/Solicitacao.java
@@ -0,0 +1,14 @@
+package igor.moreira.codeca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Solicitacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.solicitacao);
+    }
+}
